bdb9379ffb5cb043a85dad863b10132960065843
ehab <ehabmodiab@gmail.com>
2022-11-24 00:50:21 +0200

    creating alert component

---
+++ js/alert.js
@@ -0,0 +1,49 @@
+function Alertt(msg, bool) {
+  let alert = document.createElement("div");
+  alert.className = "alert";
+  alert.textContent = msg;
+  alert.style.cssText = `
+     position:absolute;
+     top:50%;
+     left:50%;
+        transform:translate(-50%,-50%);
+        background: rgba(0.47, 0.71, 0.71, 0.24);
+         box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+        backdrop-filter: blur(6.3px);
+         -webkit-backdrop-filter: blur(6.3px);
+        border: 1px solid rgba(224, 196, 196, 0.42);
+        z-index: 3;
+        color:white;
+        width: 600px;
+        height: 300px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 2rem;
+    `;
+  if (bool == true) {
+    alert.style.color = "red";
+  }
+  let close = document.createElement("span");
+  close.className = "close";
+  close.textContent = "X";
+  close.style.cssText = `
+        position: absolute;
+        top: 0;
+        right: 0;
+        padding: 10px 20px;
+        cursor: pointer;
+        color: #fff;
+        font-size: 20px;
+        font-weight: bold;
+        background: #f44336;
+        border-radius: 0 0 0 5px;
+    `;
+  close.addEventListener("click", () => {
+    alert.remove();
+  });
+  alert.append(close);
+  document.body.appendChild(alert);
+}
+
+export default Alertt;
---
+++ js/login.js
@@ -0,0 +1,52 @@
+import Alertt from "./alert.js";
+
+let email = document.querySelector("[type='email']");
+let password = document.querySelector("[type='password']");
+let login = document.querySelector("[type='submit']");
+
+let validateMsg = (input, message) => {
+  let msg = document.createElement("p");
+  msg.style.cssText = `
+    color:red;
+    font-size:11px;
+    margin:2px auto;
+    margin-bottom:-2px;
+    `;
+  msg.className = "msg";
+
+  let query = document.querySelector(".msg");
+  if (query == null) {
+    switch (input) {
+      case "email":
+        msg.textContent = message;
+        email.after(msg);
+        break;
+
+      case "password":
+        msg.textContent = message;
+        password.after(msg);
+        break;
+      default:
+        msg.textContent = message;
+        email.after(msg);
+        password.after(msg);
+    }
+  }
+};
+
+login.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (email.value == "") {
+    validateMsg("email", "Please enter your email");
+  } else if (email.value.length < 6) {
+    validateMsg("email", "Please enter a valid email");
+  }
+  if (password.value == "") {
+    validateMsg("password", "Please enter your password");
+  } else if (password.value.length < 5) {
+    validateMsg("password", "password must be at least 5 characters long");
+  } else {
+    window.location.href = "/html/items.html";
+  }
+});
