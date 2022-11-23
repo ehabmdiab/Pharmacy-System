0fd432bd5cd4deac7942739c0b42bf822e4b9e61
ehab <ehabmodiab@gmail.com>
2022-11-23 14:46:36 +0200

    adding items and invoices

---
+++ js/navbar.js
@@ -0,0 +1,12 @@
+let burger = document.getElementById("burger");
+let ul = document.querySelector(".ul");
+
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
+  ul.style.display = "flex";
+  if (!ul.classList.contains("show")) {
+    ul.style.display = "none";
+  }
+});
+
+console.log(ul);
